bffcba94af1e5a04bbb25e2793ee3c3a4a51feb5
Sergio Dominguez <sergiodr98jc@gmail.com>
2022-06-11 19:03:25 -0500

    Returning to export default

--- src/Api/controllers/user.controller.ts
+++ src/Api/controllers/user.controller.ts
@@ -5,47 +5,49 @@ import Database from 'Infrastructure/Database';
 
 import { UserService, UserServiceModel } from 'Application/Services';
 
-export const getAll =  async (_req: Request, res: Response) => {
+export default {
+  async getAll(_req: Request, res: Response) {
     const db: DatabaseModel = new Database();
     await db.connectToDb();
     const userService: UserServiceModel = new UserService();
     const usersList = await userService.getAll();
     await db.disconnect();
     res.json(usersList);
-}
+  },
 
-export const getById = async (req: Request, res: Response) => {
+  async getById(req: Request, res: Response) {
     const db: DatabaseModel = new Database();
     await db.connectToDb();
     const userService: UserServiceModel = new UserService();
     const user = await userService.getById(req.params.id);
     await db.disconnect();
     res.json(user);
-}
+  },
 
-export const create = async (req: Request, res: Response) => {
+  async create(req: Request, res: Response) {
     const db: DatabaseModel = new Database();
     await db.connectToDb();
     const userService: UserServiceModel = new UserService();
     const user = await userService.create(req.body);
     await db.disconnect();
     res.json(user);
-}
+  },
 
-export const update = async (req: Request, res: Response) => {
+  async update(req: Request, res: Response) {
     const db: DatabaseModel = new Database();
     await db.connectToDb();
     const userService: UserServiceModel = new UserService();
     const user = await userService.update(req.body);
     await db.disconnect();
     res.json(user);
-}
+  },
 
-export const remove = async (req: Request, res: Response) => {
+  async delete(req: Request, res: Response) {
     const db: DatabaseModel = new Database();
     await db.connectToDb();
     const userService: UserServiceModel = new UserService();
     const user = await userService.delete(req.params.id);
     await db.disconnect();
     res.json(user);
+  }
 }
--- src/Api/routes/userRouter.ts
+++ src/Api/routes/userRouter.ts
@@ -8,6 +8,6 @@ userRouter.get('/', userController.getAll);
 userRouter.get('/:id', userController.getById);
 userRouter.post('/', userController.create);
 userRouter.put('/:id', userController.update);
-userRouter.delete('/:id', userController.remove);
+userRouter.delete('/:id', userController.delete);
 
 export default userRouter;
